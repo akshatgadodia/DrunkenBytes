897547e0f75bba83df9380a6e4bff771f0397009
akshatgadodia <akshatgadodia@gmail.com>
2023-03-09 22:27:08 +0530

    Middleware Done

--- src/middleware.js
+++ src/middleware.js
@@ -8,9 +8,9 @@ export default async function middleware(req) {
   }
   const role = req.cookies.get("db_login");
   console.log(role?.value)
-  if (role?.value === 'true' && req.url.includes('/hold')) {
-    return NextResponse.redirect(new URL('/', req.url))
-  }
+  // if (role?.value === 'true' && req.url.includes('/hold')) {
+  //   return NextResponse.redirect(new URL('/', req.url))
+  // }
   if (role === undefined && authenticatedRoutes.includes(pathname)) {
     return NextResponse.redirect(new URL('/hold', req.url))
   }
