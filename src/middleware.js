2ff1445c9597c4568e4583f25506dd6d0565e6a0
akshatgadodia <akshatgadodia@gmail.com>
2023-03-09 22:19:40 +0530

    Middleware Updated

--- src/middleware.js
+++ src/middleware.js
@@ -7,12 +7,13 @@ export default async function middleware(req) {
     return NextResponse.next()
   }
   const role = req.cookies.get("db_login");
-  // if (role === undefined && authenticatedRoutes.includes(pathname)) {
-  //   return NextResponse.redirect(new URL('/hold', req.url))
-  // }
-  // if (role?.value === 'true' && req.url.includes('/hold')) {
-  //   return NextResponse.redirect(new URL('/', req.url))
-  // }
+  if (role === undefined && authenticatedRoutes.includes(pathname)) {
+    return NextResponse.redirect(new URL('/hold', req.url))
+  }
+  console.log(role?.value)
+  if (role?.value === 'true' && req.url.includes('/hold')) {
+    return NextResponse.redirect(new URL('/', req.url))
+  }
   
   return NextResponse.next()
 }
